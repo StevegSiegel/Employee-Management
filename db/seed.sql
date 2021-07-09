27c606be9158e5ff6b3f895b7b9ad31dfe6638f9
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-07-08 19:09:08 -0500

    created seed data for database

---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+USE employee_db;
+
+INSERT INTO departments (id, department)
+    VALUES 
+        (1, 'management'),
+        (2, 'legal'),
+        (3, 'developer');
+
+INSERT INTO roles (id, title, salary, depId)
+    VALUES
+        (1, 'mangaer', 80000.00, 1),
+        (2, 'lawyer', 100000.00, 2),
+        (3, 'developer', 70000.00, 3);
+
+INSERT INTO employees (id, firstName, lastName, roleId, managerId)
+    VALUES
+        (1, 'Billy', 'Bob', 1, NULL),
+        (2, 'Jeff', 'Jefferson', 2, 1),
+        (3, 'Steve', 'Steverson', 3, 1);
