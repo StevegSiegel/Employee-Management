e902b1240818da0ea448cf133e8af6126af47fdd
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-07-08 23:29:48 -0500

    finished but cant figure out how to link a .env file to it. so just pay no attention to the connection.js file lol

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const connection = require('./db/connection');
 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
     if (err) throw err;
     start();
 });
+
 
 function start() {
     console.log('welcome to the employee database')
     options();
 };
+
 
 function options() {
     inquirer.prompt([{
@@ -25,7 +28,7 @@ function options() {
             },
             {
                 name: 'view all roles',
-                value:'view_roles',
+                value: 'view_roles',
             },
             {
                 name: 'view all departments',
@@ -49,14 +52,14 @@ function options() {
             }
 
         ]
-    }]).then(({doThis}) => {
+    }]).then(({ doThis }) => {
         if (doThis === 'view_employees') {
             viewEmployee();
         } else if (doThis === 'view_roles') {
             viewRoles();
         } else if (doThis === 'view_dep') {
             viewDep();
-        } else if (dothis === 'add_employee') {
+        } else if (doThis === 'add_employee') {
             addEmployee();
         } else if (doThis === 'add_role') {
             addRole();
@@ -68,23 +71,26 @@ function options() {
     })
 };
 
+
 function viewEmployee() {
     console.log('selecting all employees');
-    connection.query("SELECT firstName AS `First Name`, lastName AS `Last Name`, roleId AS `Role Id` FROM employees", function(err, res) {
+    connection.query("SELECT firstName AS `First Name`, lastName AS `Last Name`, roleId AS `Role Id` FROM employees", function (err, res) {
         if (err) throw err;
         console.table(res);
         finish();
     });
 };
 
+
 function viewRoles() {
     console.log('selecting all roles');
-    connection.query('SELECT title AS `Title`, salary AS `Salary`, depId AS `Department Id` FROM roles', function(err, res) {
+    connection.query('SELECT title AS `Title`, salary AS `Salary`, depId AS `Department Id` FROM roles', function (err, res) {
         if (err) throw err;
         console.table(res);
         finish();
     });
 };
+
 
 function viewDep() {
     console.log('selecting all departments');
@@ -94,6 +100,8 @@ function viewDep() {
         finish();
     });
 };
+
+
 
 function addEmployee() {
     connection.query('SELECT id, title FROM roles', function (err, res) {
@@ -135,10 +143,11 @@ function addEmployee() {
     });
 };
 
+
 function addRole() {
     connection.query('SELECT * FROM departments', function (err, res) {
         if (err) throw err;
-        const depratments = res.map(element => {
+        const departments = res.map(element => {
             return element.id
         })
         inquirer.prompt([
@@ -155,10 +164,11 @@ function addRole() {
             {
                 type: 'list',
                 name: 'depId',
-                message: 'what department are they in'
+                message: 'what department are they in',
+                choices: departments
             }
         ]).then(function (answer) {
-            connection.query('INSERT INTO roles SET ?', answer, function(err) {
+            connection.query('INSERT INTO roles SET ?', answer, function (err) {
                 if (err) throw err;
                 console.log(`${answer.title} was added`);
                 finish();
@@ -166,3 +176,97 @@ function addRole() {
         });
     });
 };
+
+
+function addDep() {
+    connection.query('SELECT * FROM departments', function (err, res) {
+        if (err) throw err;
+        const departments = res.map(element => {
+            return element.id
+        })
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'what department are they in'
+            }
+        ]).then(function (answer) {
+            connection.query('INSERT INTO departments SET ?', answer, function (err) {
+                if (err) throw err;
+                console.log(`${answer.department} was added`);
+                finish();
+            });
+        });
+    });
+};
+
+
+
+function updateRole() {
+    connection.query("Select * from employees", function (err, res) {
+      if (err) throw err;
+      //new list of first and last names
+      const names = res.map(element => {
+        return `${element.id}: ${element.firstName} ${element.lastName}`
+      })
+      connection.query("SELECT title, id from roles", function(err, success) {
+        if (err) throw err;
+        const roles = success.map(element => element.title);  
+        inquirer.prompt([
+          {
+            name: "who",
+            type: "list",
+            choices: names,
+            message: "Whom would you like to update?"
+          }, {
+            name: "roles",
+            type: "list",
+            message: "What is the title of their new role?",
+            choices: roles
+          }
+        ]).then(answers => {
+          console.log(answers);
+          const empIdToUpdate = answers.who.split(":")[0];
+          console.log(empIdToUpdate)
+          const chosenRole = success.find(element => {
+            return element.title === answers.roles
+          });
+          console.log(chosenRole.id);
+          connection.query("UPDATE employees SET roleId=? where id=?", [chosenRole.id, empIdToUpdate], function(err, yay) {
+            if (err) throw err;
+            console.log(`role successfully changed`)
+            finish();
+          })
+          
+        })
+      })
+    })
+  
+  }
+
+function finish() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'finish',
+                message: 'would you like to continue working',
+                choices: [
+                    {
+                        name: 'yes',
+                        value: true
+                    },
+                    {
+                        name: 'no',
+                        value: false
+                    }
+                ]
+            }
+        ]).then(function (answers) {
+            if (answers.finish) {
+                options()
+            } else {
+                console.log('goodbye');
+                process.exit();
+            }
+        })
+    };
